ec039b1f81a954c9fc42064d5c1a06c1a3e142ad
Akilan R <38667273+akilan07@users.noreply.github.com>
2022-01-27 12:00:46 +0530

    Base build

---
+++ src/test/java/com/akitech/scheduler/BootSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akitech.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
